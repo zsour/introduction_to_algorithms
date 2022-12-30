9842af1ebbe6435c11b88f7ad6e1132458b5bda0
Daniel Karlsson <daniel.karlsson36@outlook.com>
2022-12-30 06:29:07 +0100

    Bucket sort fix.

--- src/pages/bucket_sort.tsx
+++ src/pages/bucket_sort.tsx
@@ -4,24 +4,24 @@ export default function BucketSort(){
     let arr = [0.44, 0.92, 0.12, 0.08, 0.89, 0.32, 0.21, 0.85, 0.53, 0.39, 0.02, 0.35, 0.18, 0.76];
 
     function insertionSort(arr: number[]){
-        for(let i = 1; i < arr.length; i++){
+        for(let i = 0; i < arr.length; i++){
             let tmp = arr[i];
             let j = i - 1;
-
             while(j >= 0 && arr[j] > tmp){
                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+                j--;
             }
         }
     }
     
-    function bucketSort(arr: number[]){
+    function bucketSort(arr: number[], numberOfBuckets: number){
         let buckets: number[][] = [];
-        for(let i = 0; i <= 10; i++){
+        for(let i = 0; i < numberOfBuckets; i++){
             buckets.push([]);
         }
 
         for(let i = 1; i <= arr.length; i++){
-            buckets[Math.floor(10 * arr[i - 1])].push(arr[i - 1]);
+            buckets[Math.floor(numberOfBuckets * arr[i - 1])].push(arr[i - 1]);
         }
 
         let result = [];
@@ -34,7 +34,7 @@ export default function BucketSort(){
     }
 
     console.time("Bucket sort");
-    let sorted = bucketSort(arr);
+    let sorted = bucketSort(arr, 5);
     console.timeEnd("Bucket sort");
     console.log(sorted);
     
